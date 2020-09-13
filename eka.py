62f5f91ebdece6cc580369962ae1e47868510e74
OwenDagobah <56173060+OwenDagobah@users.noreply.github.com>
2020-09-13 11:09:09 +0300

    Update eka.py
    Fixed typo at the line 43.
    Changed the correct defining order for the line 34.

--- eka.py
+++ eka.py
@@ -30,7 +30,8 @@ X_scaled = scaler.fit_transform(X) #Scales the tables original values.
 #Average is is 0 and standard deviation is 1.
 
 #New varables
-X_train, X_test, y_test, y_train = train_test_split(X_scaled, y, test_size=0.2)
+#NOTE: The correct order is defined at the sklearn documentation.
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2)
 #datasize 20%
 
 #Defining machine learning model
@@ -39,7 +40,7 @@ model = linear_model.LogisticRegression(multi_class='multinomial', solver='newto
 #Fit th model according to the given training data
 model.fit(X_train, y_train)
 #Predict class labels for sambles in X.
-df['Predict'] = enc.inverse_transform(model.predict(X_scaled,))
+df['Predict'] = enc.inverse_transform(model.predict(X_scaled))
 
 print('Accuracy of the train data', accuracy_score(y_train, model.predict(X_train)))
 
